8d02db42fc15ed74fbf68a66ba8828d96db86c19
Livia <livinha.guimaraes.013@gmail.com>
2024-02-03 16:26:47 -0300

    update

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,17 @@
+using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
+}
---
+++ APICatalogo/Program.cs
@@ -0,0 +1,26 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
