06d8dbc9082abb105d2e981ff387bb17d5444f27
Livia <livinha.guimaraes.013@gmail.com>
2024-02-04 19:01:37 -0300

    update

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,7 +1,10 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categorias")]
 public class Categoria
 {
     public Categoria()
@@ -9,8 +12,15 @@ public class Categoria
         Produtos = new Collection<Produto>();
     }
 
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Produto>? Produtos { get; set; }
